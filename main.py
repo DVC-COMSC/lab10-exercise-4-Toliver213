f114dd6893cc23e9134baa30061090f845f50363
Terry Oliver <126623145+Toliver213@users.noreply.github.com>
2023-03-09 23:42:24 +0000

    complete E4

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
-    pass
+  
+  celsius = float(input())
+  Fahrenheit = ((9/5)*celsius)+32
+  print (f'fahrenheit:{Fahrenheit}')
+
+  pass
 
 
 if __name__ == '__main__':
